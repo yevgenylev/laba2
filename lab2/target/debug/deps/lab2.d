5add5ee89d4fc4f8d494f55eb0bef573615fffef
yevgenylev <zhenya.levchenko2002@gmail.com>
2022-12-28 17:24:06 +0200

    lab1
    lab1

---
+++ lab2/target/debug/deps/lab2.d
@@ -0,0 +1,5 @@
+D:\os-labs-2022\lab2\target\debug\deps\lab2.exe: src\main.rs
+
+D:\os-labs-2022\lab2\target\debug\deps\lab2.d: src\main.rs
+
+src\main.rs:
---
+++ lab2/target/debug/lab2.d
@@ -0,0 +1,1 @@
+D:\os-labs-2022\lab2\target\debug\lab2.exe: D:\os-labs-2022\lab2\src\main.rs
